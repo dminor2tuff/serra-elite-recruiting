83513754461c0ec2169010bb0dc106784751e760
dminor2tuff <dsmcompton@gmail.com>
2025-12-31 16:00:41 -0800

    Update recruits.js

--- recruits.js
+++ recruits.js
@@ -49,17 +49,7 @@ function render(players) {
       : "images/placeholder.png";
 
   const position = p.Position || p.Pos || "";
-const height =
-  p.Height ||
-  p.Ht ||
-  p["Height (ft/in)"] ||
-  "";
 
-const weight =
-  p.Weight ||
-  p.Wt ||
-  p["Weight (lbs)"] ||
-  "";
 
 const writeup =
   p.WriteUp ||
@@ -71,20 +61,7 @@ const writeup =
 const hudl = p.Hudl || p.HUDL || "";
 const twitter = p.Twitter || p.X || "";
 
-card.innerHTML = `
-  <img src="${img}" class="recruit-photo" alt="${p.Name}">
 
-  <h3>${p.Name}</h3>
-
-  <p class="meta">
-    ${position} • Class of ${p.Class}
-  </p>
-
-  ${
-    height || weight
-      ? `<p class="measurements">${height}${height && weight ? " / " : ""}${weight}${weight ? " lbs" : ""}</p>`
-      : ""
-  }
 
   ${
     writeup
